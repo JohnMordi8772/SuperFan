8b92ea54a46a657b0463615a916fe092e96789c4
JOHN MORDI <john.mordi19@clcmbp13.local>
2019-04-25 08:50:23 -0500

    Initial Commit

---
+++ SuperFan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperFan
+//
+//  Created by JOHN MORDI on 4/25/19.
+//  Copyright © 2019 CLC.MORDI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
