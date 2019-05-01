fb03a954e9be9f508513c0fc68bc00783d12c33b
WILLIAM WELDER <william.welder19@clcmbp24.local>
2019-05-01 08:37:30 -0500

    beginning of arrays

--- SuperFan/ViewController.swift
+++ SuperFan/ViewController.swift
@@ -1,16 +1,13 @@
-//
-//  ViewController.swift
-//  SuperFan
-//
-//  Created by JOHN MORDI on 4/25/19.
-//  Copyright © 2019 CLC.MORDI. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
     let storage = UserDefaults.standard
+    var sports =
+    var code =
+    var points =
     @IBOutlet weak var titleLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
