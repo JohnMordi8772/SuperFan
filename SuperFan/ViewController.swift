a527248057ecda259f6f42be56ec953d539c4d4a
ALEX TIMMERMAN <alex.timmerman19@clcmb21.local>
2019-05-02 08:57:44 -0500

    Codes

--- SuperFan/ViewController.swift
+++ SuperFan/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
 
     @IBAction func createAccountButton(_ sender: UIButton) {
         let alert = UIAlertController(title: "Account Creation", message: nil, preferredStyle: .alert)
+      
     }
     
 }
