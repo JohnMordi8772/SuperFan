c6d4121ae97cbcb1d40c7f8551cc49a8d7d12b74
JOHN MORDI <john.mordi19@clcmbp13.local>
2019-05-03 08:38:27 -0500

    First part of the account creation

--- SuperFan/ViewController.swift
+++ SuperFan/ViewController.swift
@@ -13,11 +13,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func createAccountButton(_ sender: UIButton) {
+        print((storage.string(forKey: "Username") ?? "NIL") + (storage.string(forKey: "Password") ?? "NIL"))
         let alert = UIAlertController(title: "Account Creation", message: nil, preferredStyle: .alert)
         alert.addTextField(configurationHandler: nil)
-        //alert.textFields[0]
+        alert.textFields![0].placeholder = "Create Username"
         alert.addTextField(configurationHandler: nil)
+        alert.textFields![1].placeholder = "Create Password"
         alert.addTextField(configurationHandler: nil)
+        alert.textFields![2].placeholder = "Confirm Password"
+        alert.addAction(UIAlertAction(title: "Finish", style: .default, handler: {action in self.storage.set(alert.textFields![0].text, forKey: "Username"); self.storage.set(alert.textFields![2].text, forKey: "Password")}))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
     
 }
